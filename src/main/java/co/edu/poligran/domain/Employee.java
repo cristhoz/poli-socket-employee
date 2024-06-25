0f799d963b11b0fce45e8f1d396b2479a19f6812
Cristian de la Hoz <cristian@delahoz.dev>
2024-06-25 16:43:49 -0500

    Add employee domain entity

---
+++ src/main/java/co/edu/poligran/domain/Employee.java
@@ -0,0 +1,19 @@
+package co.edu.poligran.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String dateOfBirth;
+    private Integer salary;
+}
